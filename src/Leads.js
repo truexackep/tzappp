37e32025d8947d914d8ecab73b6a90a7ab6ff1d9
otricvla <truexackep@gmail.com>
2023-05-24 23:36:54 +0300

    lol

--- src/Leads.js
+++ src/Leads.js
@@ -5,34 +5,45 @@ import axios from "axios";
 
 const Leads = () => {
 
-    let [leads, setLeads] = useState();
+    let [leads, setLeads] = useState([]);
 
-   /* const loadLeads = () => {
+    const columns = [
+        {
+            field: 'id', headerName: 'ID', flex: 3
+        },
+        {
+            field: 'email', headerName: 'Email', flex: 3
+        },
+        {
+            field: 'ftd', headerName: 'Ftd', flex: 3
+        },
+        {
+            field: 'status', headerName: 'Status', flex: 3
+        },
+    ]
+
+    const loadLeads = () => {
         axios({
-            method: 'post',
-            url: 'https://crm.belmar.pro/api/v1/getstatuses',
-            headers: {
-                'token': 'ba67df6a-a17c-476f-8e95-bcdb75ed3958',
-                'Content-Type': 'application/json',
-            },
-            data: {
-                date_from: '2022-12-01 00:00:00',
-                date_to: '2022-12-31 23:59:59',
-                page: 1,
-                limit: 100,
-            }
+            method: 'get',
+            url: 'http://5.75.254.31/api/v1/test',
         }).then(response => {
-            console.log(response.data)
+            leads = response.data.data
+            setLeads([...leads])
         })
     }
 
     useEffect(() => {
         loadLeads();
-    }, [])*/
+    }, [])
 
 
-    return <div style={{height: '600px'}}>
-        <DataGrid columns={[]} rows={[]}/>
+    return <div style={{height: '600px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <Box sx={{height: '600px', width: '900px', marginTop: '120px'}}>
+            <Typography variant={'button'} sx={{fontSize: '22px'}}>
+                Lead list
+            </Typography>
+            <DataGrid columns={columns} rows={leads}/>
+        </Box>
     </div>
 }
 export default Leads;
